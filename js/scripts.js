cf2619c601949163d02cca393a218f076879aae9
Robert Sherrard <sherrard.rob+toptal@gmail.com>
2018-04-30 20:26:10 -0400

    new animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,40 @@
 var controller = new ScrollMagic.Controller();
 
-// var tl = new TimelineMax();
+var move_polygon = new TimelineMax()
+  .to("#polygon", 0.5, {attr: {points:"0,100 75,100 75,0 0,0"}});
 
-var tween1 = TweenMax.to("#polygon", 1.5, {attr: {points:"0,100 20,100 100,0 100,0 0,0"}});
-// var tween2 = tl.to("#polygon", 1.5, {attr: {points:"0,100 0,100 100,80 100,0 0,0"}});
+var scene1 = new ScrollMagic.Scene({
+    triggerElement: "#trigger1"
+  })
+  .setTween(move_polygon) // trigger a TweenMax.to tween
+  .addTo(controller);
 
-var scene = new ScrollMagic.Scene({
-          triggerElement: "#trigger1",
-          duration: "25%"
-        })
-        .setTween(tween1) // trigger a TweenMax.to tween
-        .addTo(controller);
+var scene2 = new ScrollMagic.Scene({
+    triggerElement: "#trigger2"
+  })
+  .setTween("#polygon", 0.5, {attr: {points:"0,100 100,100 100,0 0,0"}}) // trigger a TweenMax.to tween
+  .addTo(controller);
 
-var scene = new ScrollMagic.Scene({
-          triggerElement: "#trigger2",
-          duration: "50%"
-        })
-        .setTween("#polygon", 1.5, {attr: {points:"0,100 0,100 100,80 100,0 0,0"}}) // trigger a TweenMax.to tween
-        .addTo(controller);
+//
+//
+// $(function () { // wait for document ready
+// 	// init
+// 	var controller = new ScrollMagic.Controller({
+// 		globalSceneOptions: {
+// 			triggerHook: 'onLeave'
+// 		}
+// 	});
+//
+// 	// get all slides
+// 	var slides = document.querySelectorAll("section");
+//
+// 	// create scene for every slide
+// 	for (var i=0; i<slides.length; i++) {
+// 		new ScrollMagic.Scene({
+// 				triggerElement: slides[i]
+// 			})
+// 			.setPin(slides[i])
+// 			.addIndicators() // add indicators (requires plugin)
+// 			.addTo(controller);
+// 	}
+// });
